c1d557582e0836bd5b76588a8734a8d5a7d7dcf0
Jason <jason.lm@live.com>
2019-08-20 10:49:27 -0700

    Initial Commit

---
+++ DistanceConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DistanceConverter
+//
+//  Created by Jason Lee on 8/20/19.
+//  Copyright © 2019 Jason Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
