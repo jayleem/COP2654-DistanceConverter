93129755d98ec7969c1ce84eb72e10663713fd1e
Jason <jason.lm@live.com>
2019-08-20 11:46:44 -0700

    git remote add origin https://github.com/jayleem/COP2654-DistanceConverter.git git push -u origin master

--- DistanceConverter/ViewController.swift
+++ DistanceConverter/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textDist: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertDist(_ sender: UIButton) {
+        
+        guard let tempString = textDist.text else { return }
+        
+        if let distanceKm = Double(tempString) {
+            let km = distanceKm * 1.60934
+            let resultText = "KM: \(km)"
+            resultLabel.text = resultText
+        }
+    }
 }
 
